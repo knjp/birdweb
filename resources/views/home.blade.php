c5b4fdd0bb14d018ee31bb1915de0201558baf46
kiyoshi Nishikawa <kiyoshi.nishikawa@gmail.com>
2025-01-07 20:51:58 +0900

    Added a link to the analyze page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -77,7 +77,7 @@
                         <div align="center">
                             <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick="location.href='{{url('/detectbird') }}' ">
                                 <p style="">検出</p></button>
-                            <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick="location.href='{{url('/analyzebird') }}' ">
                                 <p style="">解析</p></button>
                             <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                 <p style="">Download</p></button>
