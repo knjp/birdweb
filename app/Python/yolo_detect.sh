cb8415e980d2f6cbf3c732d95c93f2bf3872b853
kiyoshi Nishikawa <kiyoshi.nishikawa@gmail.com>
2025-01-20 13:16:36 +0900

    Set YOLO not to save output video file

--- app/Python/yolo_detect.sh
+++ app/Python/yolo_detect.sh
@@ -6,4 +6,5 @@ export YOLO_CONFIG_DIR=$YOLO_CONF_PATH
 source $PDIR/venv/bin/activate
 #pip list
 #python --version
-python $PDIR/detect.py --save_video
+#python $PDIR/detect.py --save_video
+python $PDIR/detect.py
